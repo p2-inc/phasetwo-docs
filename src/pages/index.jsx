1beae549259b101669f6673dd3cb6ffb73a505d7
pnzrp2 <pnzr@phasetwo.io>
2024-11-25 12:33:22 -0700

    remove class

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -884,7 +884,7 @@ auth.init({
               <Switch
                 checked={term}
                 onChange={setTerm}
-                className="group inline-flex h-6 w-11 items-center rounded-full bg-gray-200 transition data-[checked]:bg-p2blue-600 bg-p2blue-600"
+                className="group inline-flex h-6 w-11 items-center rounded-full transition data-[checked]:bg-p2blue-600 bg-p2blue-600"
               >
                 <span className="size-4 translate-x-1 rounded-full bg-white transition group-data-[checked]:translate-x-6" />
               </Switch>
