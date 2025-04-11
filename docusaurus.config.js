8e330fe9072b907c772351a5fa05483433125a8f
pnzrp2 <pnzr@phasetwo.io>
2025-04-10 23:05:13 -0600

    fix link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -376,7 +376,7 @@ module.exports = {
             outputDir: "api", // Output directory for generated .mdx docs
             sidebarOptions: {
               groupPathsBy: "tag",
-	      categoryLinkSource: "tag",
+              categoryLinkSource: "tag",
             },
           },
         },
